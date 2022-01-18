3b5006bc2727e97b01284f78e26a0b8e9eea6813
fkuschel <fkuschel@gmx.de>
2022-01-18 23:00:40 +0100

    all

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-
+import dash
+import dash_html_components as html
 
 
 ############# RUN APP
-app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}],title='DataTown',update_title='Cargando...')
+app = dash.Dash(__name__,title='DataTown',update_title='Cargando...')
 server = app.server
 
 # Create app layout
@@ -15,8 +16,8 @@ app.layout = html.Div(
                         html.Div(
                             [
                                 html.H3(
-                                    "Municipios")))))
+                                    "Municipios")])])])])
 
 # Main
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True)
